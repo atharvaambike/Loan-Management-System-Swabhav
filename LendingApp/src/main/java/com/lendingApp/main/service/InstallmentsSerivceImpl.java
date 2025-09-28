3f1b674057af2170d61c106a26887ce383cdd57c
atharvaambike <atharvaambike@gmail.com>
2025-09-28 20:09:36 +0530

    Added base files of Services and their impl - Employee, InstallmentDuration, Installments, Transaction

---
+++ LendingApp/src/main/java/com/lendingApp/main/service/InstallmentsSerivceImpl.java
@@ -0,0 +1,10 @@
+package com.lendingApp.main.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+// @RequiredArgsConstructor
+// @Data
+public class InstallmentsSerivceImpl implements InstallmentDurationService {
+
+}
