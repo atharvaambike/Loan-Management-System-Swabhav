9b1fd022fc3335d8b61659ff034a1520bcbbd86b
atharvaambike <atharvaambike@gmail.com>
2025-09-29 11:33:55 +0530

    Small changes made

--- LendingApp/src/main/java/com/lendingApp/main/repository/ApplicationRepository.java
+++ LendingApp/src/main/java/com/lendingApp/main/repository/ApplicationRepository.java
@@ -1,5 +1,6 @@
 package com.lendingApp.main.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
--- LendingApp/src/main/java/com/lendingApp/main/service/EmployeeServiceImpl.java
+++ LendingApp/src/main/java/com/lendingApp/main/service/EmployeeServiceImpl.java
@@ -1,10 +1,20 @@
 package com.lendingApp.main.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+
+import com.lendingApp.main.entity.Employee;
 
 @Service
 // @RequiredArgsConstructor
 // @Data
 public class EmployeeServiceImpl implements EmployeeService{
+
+    @Override
+    public Employee getEmployeeById(UUID managerId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEmployeeById'");
+    }
 
 }
